e5a6585520dc29b9ed73f64557bd235ab53cf86e
Juan Venegas <j.venegasgutierrez@gmail.com>
2023-09-24 19:06:26 -0300

    Clasificador top feature y con balance

--- challenge/model.py
+++ challenge/model.py
@@ -13,8 +13,35 @@ class DelayModel:
 
     def __init__(
         self
-    ):
-        self._model = xgb.XGBClassifier(random_state=1, learning_rate=0.01) # Model should be saved in this attribute.
+    ):  
+        data=pd.read_csv('data/data.csv', low_memory=False)
+        def get_min_diff(data):
+            fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+            fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+            min_diff = ((fecha_o - fecha_i).total_seconds())/60
+            return min_diff
+
+        data['min_diff'] = data.apply(get_min_diff, axis = 1)
+        threshold_in_minutes = 15  
+        data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)     
+        
+        
+        training_data = shuffle(data[['OPERA', 'MES', 'TIPOVUELO', 'SIGLADES', 'DIANOM', 'delay']], random_state = 111)
+
+        features = pd.concat([
+        pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
+        pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
+        pd.get_dummies(data['MES'], prefix = 'MES')], 
+        axis = 1
+        )
+
+        target = data['delay']
+
+        n_y0 = len(target[target == 0])
+        n_y1 = len(target[target == 1])
+        scale = n_y0/n_y1
+
+        self._model = xgb.XGBClassifier(random_state=1, learning_rate=0.01, scale_pos_weight = scale) # Model should be saved in this attribute.
 
     def preprocess(
         self,
@@ -56,11 +83,32 @@ class DelayModel:
 
         target = data['delay']
 
-        x_train, x_test, y_train, y_test = train_test_split(features, target, test_size = 0.33, random_state = 42)
+        n_y0 = len(target[target == 0])
+        n_y1 = len(target[target == 1])
+        scale = n_y0/n_y1
+
+        top_10_features = [
+        "OPERA_Latin American Wings", 
+        "MES_7",
+        "MES_10",
+        "OPERA_Grupo LATAM",
+        "MES_12",
+        "TIPOVUELO_I",
+        "MES_4",
+        "MES_11",
+        "OPERA_Sky Airline",
+        "OPERA_Copa Air"
+        ]
+        features=features[top_10_features]
+        target=target
+        #features_validation,target_validation = train_test_split(features[top_10_features],target, test_size = 0.33, random_state = 42)
+
+        #x_train, x_test, y_train, y_test = train_test_split(features, target, test_size = 0.33, random_state = 42)
 
         
-        return x_train, x_test, y_train, y_test
-
+        #return x_train, x_test, y_train, y_test
+        return features,target
+    
     def fit(
         self,
         features: pd.DataFrame,
@@ -73,7 +121,7 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        self._model.fit(x_train,y_train)
+        self._model.fit(features,target)
         return
 
     def predict(
@@ -89,22 +137,26 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        predictions = self._model.predict(x_test)
+        predictions = self._model.predict(features)
         return predictions
 
 # Cargar tus datos en un DataFrame de pandas
-data = pd.read_csv('data/data.csv')
+data = pd.read_csv('data/data.csv', low_memory=False)
 
 # Crear una instancia de la clase DelayModel
 model = DelayModel()
 
 # Preprocesar los datos
-x_train, x_test, y_train, y_test = model.preprocess(data)
+features_validation,target_validation = model.preprocess(data,target_column="delay")
 
 # Entrenar el modelo
-model.fit(x_train, y_train)
+model.fit(features_validation, target_validation)
 
 # Realizar predicciones
-#new_data = pd.read_csv("data/data.csv")  # Reemplaza con tus nuevos datos
-predictions = model.predict(x_test)
-print(predictions)
+new_data = pd.read_csv("data/data.csv", low_memory=False)  # Reemplaza con tus nuevos datos
+predictions = model.predict(features_validation)
+#print(predictions)
+
+from sklearn.metrics import confusion_matrix, classification_report
+
+print(confusion_matrix(target_validation, predictions))
